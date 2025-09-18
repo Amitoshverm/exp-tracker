20d1a743c7ea106d40d244b595d6fb709116a78e
Amitoshverm <amitosh.verma9@gmail.com>
2025-09-18 16:04:14 +0530

    Remove dist folder from Git tracking

--- dist/users/users.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from "./users.service";
-export declare class UsersController {
-    private userService;
-    constructor(userService: UsersService);
-    getUsers(): Promise<import("./user.entity").ResponseUserDto[]>;
-    getUserById(id: number): Promise<import("./user.entity").ResponseUserDto>;
-}
